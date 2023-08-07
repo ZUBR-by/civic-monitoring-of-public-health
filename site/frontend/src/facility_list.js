c9dbcad7ff3309d18f78d556469d4b692af4cff6
Alherd <algerd.blr@gmail.com>
2023-08-07 18:14:59 +0300

    Added initial list of facilities

--- site/frontend/src/facility_list.js
+++ site/frontend/src/facility_list.js
@@ -1,11 +1,13 @@
 import { createApp, ref } from 'vue'
 
 const region = ref(null);
+const district = ref(null);
 
 createApp({
     setup() {
         return {
-            region
+            region,
+            district
         }
     }
 }).mount('#app')
